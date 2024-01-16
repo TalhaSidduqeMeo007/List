ac2ac14b11596dc0139bbec047a6de25815fcc28
talha siddiuque <talhasiddiquemeo007@gmail.com>
2024-01-16 23:44:26 +0500

    first commit

---
+++ list.dart
@@ -0,0 +1,94 @@
+// void main() {
+//   var studentNames = [1, 91, 2, 5, 6, 8, 9];
+//   studentNames.sort();
+//   print(studentNames);
+// }
+// import 'dart:html';
+
+// void main() {
+//   List studentNames = [
+//     4,
+//     5,
+//     6,
+//     7,
+//     8,
+//     9,
+//     0,
+//   ];
+//   studentNames.removeWhere((element) => element < 4);
+//   studentNames.retainWhere((Element))
+//   print(studentNames);
+// }
+
+// void main() {
+//   Map studentsInfo = {
+//     "name":"talha" ,
+//     "rollName":2323,
+//     "totalNumber":374
+//   };
+//   print(studentsInfo);
+// }
+
+// void main() {
+//   var lst = [1, 2, 3, 4, 5];
+//   var e1 = lst.first;
+//   var e2 = lst.last;
+//   var e3 = lst.elementAt(1);
+//   var len = lst.length;
+
+//   print('there are $len elements in the list');
+//   print('the first element is $e1');
+//   print('the last element is $e2');
+//   print('the second elemnet is $e3');
+//}
+
+// void main() {
+//   var mylist = [0, 'one', 'two', 'three', 'four', 'five'];
+//   mylist[3] = 4;
+//   print(mylist);
+//   mylist.replaceRange(1, 2, [3]);
+// }
+
+// void main() {
+//   var intList = [0, 5, 3, 6, 8, 17, 78];
+//   intList.sort();
+//   print('intList');
+// }
+
+// void main() {
+//   double prize = 12343.0498;
+//   print(prize.toStringAsFixed(4));
+// }
+
+// void main() {
+//   print("I m \nUS");
+//   print("I m \nUS");
+// }
+
+//  void main() {
+// // Set prize value
+// num prize = 10;
+// String withoutRawString = "The value of prize is \t $prize"; // regular String
+// String withRawString =r"The value of prize is \t $prize"; // raw String
+
+// print("Without Raw: $withoutRawString"); // regular result
+// print("With Raw: $withRawString"); // with raw result
+
+// }
+
+// void main() {
+// int one = 1;
+// print("Type of one is ${one.runtimeType}");
+// String oneInString = one.toString(); 
+// print("Value of oneInString is $oneInString");
+// // this will print data type
+// print("Type of oneInString is ${oneInString.runtimeType}");
+// }
+
+void main() { 
+   double num1 = 10.01;
+   int num2 = num1.toInt(); // converting double to int
+
+  print("The value of num1 is $num1. Its type is ${num1.runtimeType}");
+  print("The value of num2 is $num2. Its type is ${num2.runtimeType}");
+}
